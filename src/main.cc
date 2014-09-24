e9e8db268c4a21c389eccd94f0b0ed97c904dc1d
Konstantin Nikitin <stromsund@yandex-team.ru>
2014-09-24 21:53:31 +0400

    Handling open repo errors

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,10 @@
 int main() {
   git_repository* repo = NULL;
   int error = git_repository_open_bare(&repo, "/home/stromsund/Development/request-by-commit/test/testrepo_bare");
+  if (error) {
+    std::cerr << "Error on opening repo! (" << error << ")\n";
+    return 1;
+  }
   std::cout << error << "\n";
   return 0;
 }
