02fb28d5e640872e6044b0c087e6bfc0b1560d50
Konstantin Nikitin <stromsund@yandex-team.ru>
2014-10-16 18:25:31 +0400

    Bulk update

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 #include "git2.h"
+
+struct GitOidHasher {
+  size_t operator()(const git_oid& oid) const {
+    std::vector<char> str(40, '\0');
+    git_oid_fmt(&str[0], &oid);
+    std::string id(&str[0]);
+    std::cout << "Commit Id is " << id << '\n';
+    return str[0];
+  }  
+};
+
+typedef std::unordered_map<git_oid, unsigned char, GitOidHasher> ParentCounter;
+
+bool operator==(const git_oid& lfs, const git_oid& rhs) {
+  return git_oid_equal(&lfs, &rhs);
+}
+
+
+bool is_merge_commit(const git_oid& oid,
+                     git_repository* repo,
+                     ParentCounter* parent_counter) {
+  ParentCounter::const_iterator element = parent_counter->find(oid);
+  size_t parent_count;
+  if (element == parent_counter->end()) {
+    git_commit* commit;
+    git_commit_lookup(&commit, repo, &oid);    
+    parent_count = git_commit_parentcount(commit);
+    (*parent_counter)[oid] = parent_count;
+    git_commit_free(commit);
+  } else {
+    std::cout << "Cache pick!\n";
+    parent_count = element->second;
+  }
+  return parent_count > 1;
+}
+
+bool traverse_children(const git_oid& current,
+                       const git_oid& parent,
+		       git_repository* repo,
+		       ParentCounter* parent_counter) {
+  git_commit* commit;
+  git_commit_lookup(&commit, repo, &current);    
+  size_t parent_count = git_commit_parentcount(commit);
+  for (size_t parent_index = 0; parent_index < parent_count; ++parent_index) {
+    const git_oid* parent_id = git_commit_parent_id(commit, parent_index);
+    if (!is_merge_commit(*parent_id, repo, parent_counter)) {
+	    std::cout << "Traverce child\n";
+      traverse_children(*parent_id, parent, repo, parent_counter);
+    }
+  }
+  git_commit_free(commit);
+}
 
 int show_ref(git_reference *ref, void *data) {
   std::cout << "Next ref\n";
@@ -8,6 +62,7 @@ int show_ref(git_reference *ref, void *data) {
 }
 
 int main() {
+  std::unordered_map<git_oid, unsigned char, GitOidHasher> parent_count;
   git_threads_init();
   git_repository* repo = NULL;
   int error = git_repository_open_bare(&repo, "/home/stromsund/Development/request-by-commit/test/testrepo_bare");
@@ -23,6 +78,16 @@ int main() {
   git_oid oid;
   int counter = 0;
   while (!git_revwalk_next(&oid, walker)) {
+    if (!is_merge_commit(oid, repo, &parent_count)) {
+	    traverse_children
+		    (oid, oid, repo, &parent_count);
+	    continue;
+    }
+    git_commit* commit;
+    git_commit_lookup(&commit, repo, &oid);
+    git_commit_free(commit);
+    parent_count[oid] = git_commit_parentcount(commit);
+    std::cout << git_commit_parentcount(commit) << ' ' << git_commit_message(commit) << '\n';
     counter += 1;
   }
   git_revwalk_free(walker);
